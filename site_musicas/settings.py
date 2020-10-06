a3b151553564c0530a5a81b9fff2b1a9cac92212
Pedroflp <pedroflp@ufrn.edu.com.br>
2020-10-06 15:24:13 -0300

    Config Heroku

--- site_musicas/settings.py
+++ site_musicas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'musicas/media')
+
+django_heroku.settings(locals())
